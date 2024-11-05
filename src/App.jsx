93aa8428b25087ffa7c01be26ece742837643f26
GabyScript <gabrielpreller@gmail.com>
2024-11-04 21:52:07 -0300

    Img Fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className='h-svh flex flex-col items-center justify-center bg-green-600'>
-        <img src='./src/assets/android-chrome-512x512.png'/>
+        <img src='./android-chrome-512x512.png'/>
         <h1 className="text-3xl font-bold underline align-middle">
           Sitio en construccion
         </h1>
