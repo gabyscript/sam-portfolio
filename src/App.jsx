4397a01b21e4cbda4a35247e4f65b0583601710a
GabyScript <gabrielpreller@gmail.com>
2024-11-05 17:50:55 -0300

    Se agrega avances en NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './layout/navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='h-svh flex flex-col items-center justify-center bg-green-600'>
-        <img src='./android-chrome-512x512.png'/>
-        <h1 className="text-3xl font-bold underline align-middle">
-          Sitio en construccion
-        </h1>
-      </div>      
+      <NavBar />  
     </>
   )
 }
